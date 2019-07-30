36d63222c3177de6df25369a70ca2ff51c3f6b0d
Giacomo Giudice <giacomo.giudice@mail.com>
2019-07-30 17:23:01 +0200

    Add new 'fixedpoint.m'

--- umps/fixedpoint.m
+++ umps/fixedpoint.m
@@ -1,21 +1,43 @@
-function [rho,eta,rho_adj] = fixedpoint(A,direction)
-% Finds right fixedpoint by default
-rho_adj = [];
-if nargin < 2
-	direction = 'r';
-elseif strcmp(direction,'all')
-	[rho,eta] = fixedpoint(A,'l');
-	[rho_adj,eta_adj] = fixedpoint(A,'r');
-	rho_norm = sqrt(trace(rho*rho_adj.'));
-	rho = rho/rho_norm;
-	rho_adj = rho_adj/rho_norm;
-	eta = mean([eta,eta_adj]);
-	return
+function [rho,eta] = fixedpoint(A_1,O,A_2,direction,settings)
+
+if exist('settings','var') && isfield(settings,'eigsolver')
+	eigsolver = settings.eigsolver.handle;
+	eigsolver_options = settings.eigsolver.options;
+else
+	eigsolver = @eigs;
+	eigsolver_options.isreal = isequal(A_1,A_2) && all([isreal(A_1),isreal(O)]);
 end
-D = size(A,1);
-options.isreal = isreal(A);
-options.issym = 0;
-[rho,eta] = eigs(@(v) applyTv(v,A,1,A,direction),D^2,1,'lm',options);
-rho = reshape(rho,[D,D]);
-rho = rho/trace(rho);
+eigsolver_mode = 'lm';
+eigsolver_options.issym = false;
+eigsolver_options.fail = 'keep';
+if iscell(A_1) && iscell(A_2)
+	D_1 = size(A_1{1},1);
+	D_2 = size(A_2{1},1);
+else
+	D_1 = size(A_1,1);
+	D_2 = size(A_2,1);
+end
+if ndims(O) == 4
+	chi = size(O,1);
+else
+	chi = 1;
+end
+fun = @(v) applyTv(v,A_1,O,A_2,direction);
+if iscell(A_1) && iscell(O) && ~iscell(O{1})
+	fun = @(v) applyTvNtimes(v,A_1,H,A_2,direction);
+end
+[rho,eta] = eigsolver(fun,D_1*D_2*chi,1,eigsolver_mode,eigsolver_options);
+rho = reshape(rho,[D_1,D_2,chi]);
+end
+
+function v = applyTvNtimes(v,A1,H,A2,direction)
+N = length(A1);
+if direction == 'l'
+	ind = 1:N;
+elseif direction == 'r'
+	ind = N:(-1):1;
+end
+for n = ind
+	v = applyTv(v,A1{n},H{n},A2{n},direction);
+end
 end
